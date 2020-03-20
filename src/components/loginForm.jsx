35d7eb7d54246ea9ef4194477cb1569baa8b9f12
Leandro Hurtado <leandro.hurtado@bolivia.assuresoft.com>
2020-03-20 16:53:32 -0400

    made succesful get and post

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,29 +2,67 @@ import React, { Component } from 'react';
 import userImg from '../images/userImage.png';
 
 class LoginForm extends Component {
-    state = {  }
+    state = { contacts: [] }
     render() { 
+        console.log(this.state.contacts.id);
         return (
             <React.Fragment>
-                <form method="post">
+                <form id="StudentForm" onSubmit={this.handleSubmit} /*method="get"*/ >
                     <div className="imgContainter">
                         <img src={userImg} alt="Student" className="avatar"/>
                     </div>
                     <div className= "containerForm">
-                        <label><b>Username</b></label>
-                        <input type="text" placeholder="Enter Username" name= "uname" required />
-                        
-                        <label><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name= "psw" required />
-                        
-                        <button type= "submit">Login</button>
+                        <label><b>Username: </b></label>
+                        <input type="text" name="uname" placeholder="Enter Username" />
+                        <br></br>
+                        <label><b>Password: </b></label>
+                        <input type="password" placeholder="Enter Password" required />
+                        <br></br>
+                        <button type="submit" >Login</button>
                     </div>
-                    <div className="containerForm" style="background-color:#f1f1f1">
+                    <div className="containerForm" >
                         <button type= "button" className="cancelBtn">Cancel</button>
                     </div>
                 </form>
+                
             </React.Fragment>
         );
+    }
+    handleSubmit =(event) =>
+    {
+        event.preventDefault();
+        let name = event.target.uname.value;
+        fetch('http://localhost:51061/api/Users/'+name)
+        .then(result=>result.json())
+        .then((data)=>{
+            this.setState({contacts: data})
+        })
+        .catch(this.handleCatch);
+        
+    }
+    handleCatch = ()=>
+    {
+        alert("Incorrect username or password!");
+        document.getElementById("StudentForm").reset();
+    }
+    handlePost = (event) =>
+    {
+        event.preventDefault();
+        fetch('http://localhost:51061/api/Users/',
+        {
+            method: 'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: 3,
+                name: 'Leandro',
+                email: 'leandro@bolivia.com',
+                phone: '888888',
+                role: 10
+            })
+        })
     }
 }
  
